923c46b2b8bfe0ee79c45b1fc3b09065c97d2a7b
tomsloj <tomsloj@gmail.com>
2019-05-23 09:37:08 +0200

    fix tests

--- test/test.cpp
+++ test/test.cpp
@@ -17,7 +17,7 @@ BOOST_AUTO_TEST_CASE( testSetAndGetPESEL1 )
     Student student;
     std::string pesel = "99992222000";
     student.setPESEL( pesel );
-    BOOST_CHECK( student.getPESEL() == "99992222000" );
+    BOOST_CHECK( student.getPESEL() == pesel );
 }
 BOOST_AUTO_TEST_CASE( testSetAndGetPESEL2 )
 {
@@ -25,20 +25,13 @@ BOOST_AUTO_TEST_CASE( testSetAndGetPESEL2 )
     student.setPESEL( "00000000000" );
     BOOST_CHECK( student.getPESEL() == "00000000000" );
 }
-BOOST_AUTO_TEST_CASE( testSetAndGetPESEL3 )
-{
-    Student student;
-    std::string pesel = "01234567890";
-    student.setPESEL( pesel );
-    BOOST_CHECK( student.getPESEL() == "01234567890" );
-}
 
 BOOST_AUTO_TEST_CASE( testSetAndGetName1 )
 {
     Student student;
     std::string name = "John";
     student.setName( name );
-    BOOST_CHECK( student.getName() == "John" );
+    BOOST_CHECK( student.getName() == name );
 }
 BOOST_AUTO_TEST_CASE( testSetAndGetName2 )
 {
@@ -50,9 +43,9 @@ BOOST_AUTO_TEST_CASE( testSetAndGetName2 )
 BOOST_AUTO_TEST_CASE( testSetAndGetSurname1 )
 {
     Student student;
-    std::string name = "Johanson";
-    student.setName( name );
-    BOOST_CHECK( student.getName() == "Johanson" );
+    std::string surname = "Johanson";
+    student.setName( surname );
+    BOOST_CHECK( student.getName() == surname );
 }
 BOOST_AUTO_TEST_CASE( testSetAndGetSurname2 )
 {
@@ -69,39 +62,46 @@ BOOST_AUTO_TEST_CASE( testSetStudent1 )
 {
     Student student;
     student.set_student( "09876543211", "Andrew", "Dude", 123 );
-    BOOST_CHECK( student.getPESEL() == "09876543211" && student.getName() == "Andrew"
-        && student.getSurname() == "Dude" && student.getIQ() == 123 );
+    BOOST_CHECK( student.getPESEL() == "09876543211" );
+    BOOST_CHECK( student.getName() == "Andrew" );
+    BOOST_CHECK( student.getSurname() == "Dude" );
+    BOOST_CHECK( student.getIQ() == 123 );
 }
 BOOST_AUTO_TEST_CASE( testSetStudent2 )
 {
     Student student;
     student.set_student( "56294643921", "Martin", "Smith", 45 );
-    BOOST_CHECK( student.getPESEL() == "56294643921" && student.getName() == "Martin"
-        && student.getSurname() == "Smith" && student.getIQ() == 45 );
+    BOOST_CHECK( student.getPESEL() == "56294643921" );
+    BOOST_CHECK( student.getName() == "Martin" );
+    BOOST_CHECK( student.getSurname() == "Smith" );
+    BOOST_CHECK( student.getIQ() == 45 );
 }
 
 BOOST_AUTO_TEST_CASE( testIncreaseIQ1 )
 {
     Student student;
     student.set_student( "09876543211", "Andrew", "Dude", 123 );
+    BOOST_CHECK_EQUAL( student.getIQ(), 123 );
     ++student;
-    BOOST_CHECK( student.getIQ() == 124 );
+    BOOST_CHECK_EQUAL( student.getIQ(), 124 );
 }
 BOOST_AUTO_TEST_CASE( testIncreaseIQ2 )
 {
     Student student;
     student.set_student( "56294643921", "Martin", "Smith", 45 );
+    BOOST_CHECK_EQUAL( student.getIQ(), 45 );
     ++student;
     ++student;
     ++student;
     ++student;
     ++student;
-    BOOST_CHECK( student.getIQ() == 50 );
+    BOOST_CHECK_EQUAL( student.getIQ(), 50 );
 }
 BOOST_AUTO_TEST_CASE( testIncreaseIQ3 )
 {
     Student student;
     student.set_student( "09876543211", "Andrew", "Dude", 123 );
+    BOOST_CHECK_EQUAL( student.getIQ(), 123 );
     for ( int i = 0; i < 783; ++i )
     {
         ++student;
